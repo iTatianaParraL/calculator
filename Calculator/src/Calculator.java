6f22ffba7361360935f207cce9b4853971bf3e29
iTatianaParraL <itatianaparral@gmail.com>
2024-06-15 21:48:38 -0500

    add multiplication + division methods
    added in advanced branch

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+public double multiplication (double a, double b){
+    
+    return a * b;
+}
+
+public double Division (double a, double b){
+    
+    return a / b;
+}
+
+    
+}
