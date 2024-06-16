343ce0073029766472009d1f6028f5f31eaaae90
iTatianaParraL <itatianaparral@gmail.com>
2024-06-15 21:51:23 -0500

    add methods add + substract
    added to branch base_calculator

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public double add (double a, double b){
+        
+        return a + b;
+    }
+    
+    public double substract (double a, double b){
+        
+        return a - b;
+    }
+
+}
