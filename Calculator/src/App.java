eedb1bc27ad13899ef1cd083a0248b6a5d441673
iTatianaParraL <itatianaparral@gmail.com>
2024-06-18 13:50:14 -0500

    Update App.java

--- Calculator/src/App.java
+++ Calculator/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, This is my new calculator!");
     }
 }
